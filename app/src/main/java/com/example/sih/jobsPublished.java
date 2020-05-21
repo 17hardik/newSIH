cc1c7f0eb7d962f92ce7cbcd4fd39509a92c88df
Himanshu Gangil <himanshugangil1999@gmail.com>
2020-05-22 01:24:09 +0530

    Look out for crash

---
+++ app/src/main/java/com/example/sih/jobsPublished.java
@@ -0,0 +1,14 @@
+package com.example.sih;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jobsPublished extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jobs_published);
+    }
+}
