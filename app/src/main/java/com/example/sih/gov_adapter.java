de95c84f19451e17e4b087071e27c1ccea230184
Himanshu Gangil <himanshugangil1999@gmail.com>
2020-07-17 14:00:05 +0530

    Tried to correct functionality of SearchView in ListView

--- app/src/main/java/com/example/sih/gov_adapter.java
+++ app/src/main/java/com/example/sih/gov_adapter.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
